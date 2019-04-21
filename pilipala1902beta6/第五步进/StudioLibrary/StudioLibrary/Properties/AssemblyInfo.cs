e0ee7e804b021c6e768dcd72a715d633cee62365
Thaumy <Studio_ai@outlook.com>
2019-04-21 10:02:49 +0800

    1

---
+++ pilipala1902beta6/第五步进/StudioLibrary/StudioLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("StudioLibrary")]
+[assembly: AssemblyDescription("Thaumy's Web Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ARCANEMATRIX阵方互联")]
+[assembly: AssemblyProduct("StudioLibrary")]
+[assembly: AssemblyCopyright("Copyright©2016-2019")]
+[assembly: AssemblyTrademark("ARCANEMATRIX阵方互联")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("be8bd0c2-ef6e-4e06-bb12-bf11c8170f91")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
