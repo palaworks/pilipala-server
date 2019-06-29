16d16bc8ba71739a391fb21c69fdeb585d9b695a
Thaumy <Studio_ai@outlook.com>
2019-06-29 23:50:28 +0800

    1

---
+++ pilipala1902beta6/第七步进/field/ui_js/hiWord.js
@@ -0,0 +1,7 @@
+﻿var typed = new Typed('.hiWord', {
+    strings: ['你好，我叫^300Thaumy', '很高兴^150你能来我的^150小破站瞧一瞧！^200', '平时没什么爱好，^150喜欢..', '平时没什么爱好，喜欢..<br/>^700rua', '平时没什么爱好，喜欢..<br/>敲敲代码', '这里^150是我的小破站', '又一个码农的家.', '又一个码农的家..', '又一个码农的家...'],
+    typeSpeed: 100,
+    backSpeed: 0,
+    smartBackspace: true,
+    loop: false
+});
