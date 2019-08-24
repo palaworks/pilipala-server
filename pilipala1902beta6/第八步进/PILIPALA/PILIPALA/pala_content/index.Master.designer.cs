69d18d869a46ef500ef55ba622ebb33e124a8c3f
Thaumy <Studio_ai@outlook.com>
2019-08-25 06:02:16 +0800

    1

--- pilipala1902beta6/第八步进/PILIPALA/PILIPALA/pala_content/index.Master.designer.cs
+++ pilipala1902beta6/第八步进/PILIPALA/PILIPALA/pala_content/index.Master.designer.cs
@@ -13,15 +13,6 @@ namespace PILIPALA {
     public partial class index {
         
         /// <summary>
-        /// indexHead 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder indexHead;
-        
-        /// <summary>
         /// indexBody 控件。
         /// </summary>
         /// <remarks>
