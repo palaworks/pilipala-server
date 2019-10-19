806febcf4ef8023592f238e8f741cd7aff7e5bf9
Thaumy <Studio_ai@outlook.com>
2019-10-20 07:22:06 +0800

    1

---
+++ pilipala1908beta7/Build_1/PILIPALA/PILIPALA/pala_custom/theme/field2/cut/FRAME.Master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PILIPALA.pala_custom.theme.field2.cut
+{
+    public partial class FRAME : System.Web.UI.MasterPage
+    {
+        /* 创建pala基本服务 */
+        protected pala_system.service.Basic BS { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /* 初始化基本服务 */
+            BS = new pala_system.service.Basic();
+        }
+    }
+}
