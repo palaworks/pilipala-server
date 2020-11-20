7bdfeaea8916e5b87f7aa3f1e5dc912eb4cebde2
Thaumy Cheng <Studio_ai@outlook.com>
2020-11-20 20:48:09 +0800

    工作负载迁移&内核更新

--- pilipala2008beta8/Build1/jiligulu/src/views/PostList.vue
+++ pilipala2008beta8/Build1/jiligulu/src/views/PostList.vue
@@ -10,77 +10,77 @@
     >
       <v-icon>mdi-plus</v-icon>
     </v-btn>
-
-    <v-card v-for="item in post_list" :key="item.ID" class="mx-auto mt-3" width="92%">
-      <v-list-item>
-        <v-list-item-content>
-          <p
-            style="overflow:hidden;text-overflow:ellipsis;white-space:nowrap;"
-            v-if="item.Type=='note'?false:true"
-            class="text-h6"
-          >{{ item.Title }}</p>
-          <p v-if="item.Type=='note'?false:true" class="text-subtitle-2">{{ item.Summary }}</p>
-          <p
-            style="max-height:56px;max-width:60vw;display:-webkit-box;overflow:hidden;text-overflow:ellipsis;word-wrap:break-word;white-space:normal;-webkit-line-clamp:2;-webkit-box-orient:vertical;"
-            :class="ContentClass(item.Type)"
-          >
-            <v-icon v-if="item.Type=='note'?true:false" left class="mt-n1">mdi-label</v-icon>
-            {{ item.Content }}
-          </p>
-        </v-list-item-content>
-        <v-list-item-action>
-          <div>
-            <v-chip
-              v-if="item.Mode=='o'?false:true"
-              text-color="white"
-              small
-              :color="item.Mode=='archived'?'amber accent-4':item.Mode=='sche'?'blue accent-5':item.Mode=='x'?'grey':null"
-            >{{item.Mode=='archived'?'已归档':item.Mode=='sche'?'计划':item.Mode=='x'?'隐藏':null}}</v-chip>
-            <v-btn disabled text class="text--disabled">
-              <v-icon left>mdi-folder-outline</v-icon>
-              {{item.Archiv}}
+    <v-lazy min-height="100" transition="fade-transition" v-for="item in post_list" :key="item.ID">
+      <v-card class="mx-auto mt-3" width="92%">
+        <v-list-item>
+          <v-list-item-content>
+            <p
+              style="overflow:hidden;text-overflow:ellipsis;white-space:nowrap;"
+              v-if="item.Type=='note'?false:true"
+              class="text-h6"
+            >{{ item.Title }}</p>
+            <p v-if="item.Type=='note'?false:true" class="text-subtitle-2">{{ item.Summary }}</p>
+            <p
+              style="max-height:56px;max-width:60vw;display:-webkit-box;overflow:hidden;text-overflow:ellipsis;word-wrap:break-word;white-space:normal;-webkit-line-clamp:2;-webkit-box-orient:vertical;"
+              :class="ContentClass(item.Type)"
+            >
+              <v-icon v-if="item.Type=='note'?true:false" left class="mt-n1">mdi-label</v-icon>
+              {{ item.Content }}
+            </p>
+          </v-list-item-content>
+          <v-list-item-action>
+            <div>
+              <v-chip
+                v-if="item.Mode=='o'?false:true"
+                text-color="white"
+                small
+                :color="item.Mode=='archived'?'amber accent-4':item.Mode=='sche'?'blue accent-5':item.Mode=='x'?'grey':null"
+              >{{item.Mode=='archived'?'已归档':item.Mode=='sche'?'计划':item.Mode=='x'?'隐藏':null}}</v-chip>
+              <v-btn disabled text class="text--disabled">
+                <v-icon left>mdi-folder-outline</v-icon>
+                {{item.Archiv}}
+              </v-btn>
+              <v-btn text class="text--disabled">{{item.ID}}</v-btn>
+            </div>
+            <v-btn small text class="text--disabled">
+              <v-icon small>mdi-account-edit</v-icon>
+              {{item.User}}
+              <v-icon small class="ml-2">mdi-account-multiple-outline</v-icon>
+              {{item.UVCount}}
+              <v-icon small class="ml-2">mdi-star-outline</v-icon>
+              {{item.StarCount}}
             </v-btn>
-            <v-btn text class="text--disabled">{{item.ID}}</v-btn>
-          </div>
-          <v-btn small text class="text--disabled">
-            <v-icon small>mdi-account-edit</v-icon>
-            {{item.User}}
-            <v-icon small class="ml-2">mdi-account-multiple-outline</v-icon>
-            {{item.UVCount}}
-            <v-icon small class="ml-2">mdi-star-outline</v-icon>
-            {{item.StarCount}}
+            <v-btn disabled small text>最后编辑 : {{item.LCT}}</v-btn>
+          </v-list-item-action>
+        </v-list-item>
+        <v-card-actions class="mt-n7">
+          <v-btn
+            :to="{ name: 'Edit', params: { post_id:item.ID}}"
+            class="text-caption"
+            color="blue"
+            text
+          >编辑</v-btn>
+
+          <v-btn :to="{ name: 'Iteration', params: { post_id:item.ID}}" class="text-caption" text>
+            <v-icon color="secondary" left>mdi-source-merge</v-icon>迭代化
           </v-btn>
-          <v-btn disabled small text>最后编辑 : {{item.LCT}}</v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-card-actions class="mt-n7">
-        <v-btn
-          :to="{ name: 'Edit', params: { post_id:item.ID}}"
-          class="text-caption"
-          color="blue"
-          text
-        >编辑</v-btn>
 
-        <v-btn :to="{ name: 'Iteration', params: { post_id:item.ID}}" class="text-caption" text>
-          <v-icon color="secondary" left>mdi-source-merge</v-icon>迭代化
-        </v-btn>
+          <v-tooltip right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text class="text-caption" v-bind="attrs" v-on="on">详情</v-btn>
+            </template>
+            创建时间 : {{item.CT}}
+            <br />
+            MD5 : {{item.MD5}}
+          </v-tooltip>
 
-        <v-tooltip right>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn text class="text-caption" v-bind="attrs" v-on="on">详情</v-btn>
-          </template>
-          创建时间 : {{item.CT}}
-          <br />
-          MD5 : {{item.MD5}}
-        </v-tooltip>
-
-        <v-row justify="end">
-          <v-btn small class="text-caption mr-4" color="error" text @click="Dispose(item.ID)">删除</v-btn>
-        </v-row>
-      </v-card-actions>
-    </v-card>
-
-    <v-sheet class="mb-3"></v-sheet>
+          <v-row justify="end">
+            <v-btn small class="text-caption mr-4" color="error" text @click="Dispose(item.ID)">删除</v-btn>
+          </v-row>
+        </v-card-actions>
+      </v-card>
+    </v-lazy>
+    <v-sheet height="68px"></v-sheet>
   </v-app>
 </template>
 
@@ -108,7 +108,7 @@ export default {
     Dispose: function (ID) {
       this.$axios({
         method: "post",
-        url: "https://localhost:44334/system/serv/SysServ.asmx/Dispose",
+        url: "https://localhost:44334/system/serv/SysServ.asmx/Post_Dispose",
         data: qs.stringify({
           ID: ID,
         }),
@@ -123,7 +123,7 @@ export default {
     },
     GetData: function () {
       this.$axios
-        .post("https://localhost:44334/system/serv/SysServ.asmx/GetPosts")
+        .post("https://localhost:44334/system/serv/SysServ.asmx/Get_Post_DataList")
         .then((response) => (this.post_list = response.data))
         .catch(function (error) {
           // 请求失败处理
