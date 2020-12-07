31f5b8174367714b5dca3f4a1100b31c064005b2
Thaumy Cheng <Studio_ai@outlook.com>
2020-12-07 23:29:48 +0800

    常规维护
    新增可迭代对象CommentSet(评论数据集)，此对象可提供较泛型集合更专一化的数据聚合功能
    Html过滤器现在支持对转义字符的过滤
    在内核层面提供了对文章原数据的格式化支持
    现已在内核层面提供了对文章内容的Html过滤和Markdown转义支持
    简化了FieldV3主题的一些样式函数

---
+++ pilipala2008beta8/Build1/PILIPALA/PILIPALA/system/Formatter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PILIPALA.system
+{
+    public static class Formatter
+    {
+        /// <summary>
+        /// 转到中文时间概要（今天、昨天、上个月...）
+        /// </summary>
+        /// <param name="dateTime">要计算的时间</param>
+        /// <returns></returns>
+        public static string CN_TimeSummary(DateTime dateTime)
+        {
+            //获取当前时间
+            DateTime DateTime1 = DateTime.Now;
+
+            TimeSpan ts1 = new TimeSpan(DateTime1.Ticks);
+            TimeSpan ts2 = new TimeSpan(dateTime.Ticks);
+
+            //时间比较，得出差值
+            TimeSpan ts = ts1.Subtract(ts2).Duration();
+
+            if (ts.Days >= 60)
+            {
+                return null;
+            }
+            else
+            if (ts.Days >= 50 && ts.Days < 80)
+            {
+                return "两月前";
+            }
+            else
+            if (ts.Days >= 28 && ts.Days < 50)
+            {
+                return "一月前";
+            }
+            else
+            if (ts.Days >= 21 && ts.Days < 28)
+            {
+                return "三周前";
+            }
+            else
+            if (ts.Days >= 14 && ts.Days < 21)
+            {
+                return "两周前";
+            }
+            else
+            if (ts.Days >= 7)
+            {
+                return "一周前";
+            }
+            else
+            if (ts.Days < 1 && ts.Hours < 6)
+            {
+                return "刚刚";
+            }
+            else
+            {
+                switch (ts.Days)
+                {
+                    case 0: return "今天";
+                    case 1: return "昨天";
+                    case 2: return "两天前";
+                    case 3: return "三天前";
+                    case 4: return "四天前";
+                    case 5: return "五天前";
+                    case 6: return "六天前";
+                    default: return null;
+                }
+            }
+        }
+        /// <summary>
+        /// 转到中文星期（星期三、星期五...）
+        /// </summary>
+        /// <param name="DateTime"></param>
+        /// <returns></returns>
+        public static string CN_Week(DateTime DateTime)
+        {
+            string[] Day = new string[] { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+            return Day[Convert.ToInt16(DateTime.DayOfWeek)] + " " + DateTime.Hour + ":" + DateTime.Minute;
+        }
+    }
+}
---
+++ pilipala2008beta8/Build1/WaterLibrary/WaterLibrary/com/CommentLake.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using MySql.Data.MySqlClient;
+
+using WaterLibrary.com.MySQL;
+using WaterLibrary.com.pilipala;
+using WaterLibrary.stru.MySQL;
+using WaterLibrary.stru.CommentLake;
+using WaterLibrary.stru.CommentLake.CommentKey;
+using WaterLibrary.stru.pilipala.DB;
+using WaterLibrary.stru.pilipala.Post;
+
+namespace WaterLibrary.com.CommentLake
+{
+    /// <summary>
+    /// 评论湖
+    /// </summary>
+    public class CommentLake
+    {
+        /// <summary>
+        /// 表集
+        /// </summary>
+        private PLTables Tables { get; set; }
+        /// <summary>
+        /// MySql数据库管理器
+        /// </summary>
+        private MySqlManager MySqlManager { get; set; }
+
+        /// <summary>
+        /// 准备评论湖
+        /// </summary>
+        /// <param name="CORE"></param>
+        public void Ready(CORE CORE)
+        {
+            Tables = CORE.Tables;
+            MySqlManager = CORE.MySqlManager;
+        }
+
+        /// <summary>
+        /// 得到最大评论ID（私有）
+        /// </summary>
+        /// <returns>不存在返回1</returns>
+        private int GetMaxCommentID()
+        {
+            string SQL = string.Format("SELECT max(CommentID) FROM {0}", Tables.Comment);
+
+            object MaxCommentID = MySqlManager.GetKey(SQL);
+            return MaxCommentID == DBNull.Value ? 1 : Convert.ToInt32(MaxCommentID);
+        }
+        /// <summary>
+        /// 获得目标文章下的最大评论楼层（私有）
+        /// </summary>
+        /// <returns>不存在返回1</returns>
+        private int GetMaxFloor(int PostID)
+        {
+            string SQL = string.Format("SELECT max(Floor) FROM {0} WHERE PostID = {1}", Tables.Comment, PostID);
+
+            object MaxFloor = MySqlManager.GetKey(SQL);
+            return MaxFloor == DBNull.Value ? 1 : Convert.ToInt32(MaxFloor);
+        }
+
+        /// <summary>
+        /// 评论总计数
+        /// </summary>
+        public int TotalCommentCount
+        {
+            get
+            {
+                object Count = MySqlManager.GetKey(string.Format("SELECT COUNT(*) FROM {0}", Tables.Comment));
+                return Count == DBNull.Value ? 0 : Convert.ToInt32(Count);
+            }
+        }
+        /// <summary>
+        /// 得到目标文章的评论计数
+        /// </summary>
+        /// <param name="PostID">目标文章ID</param>
+        /// <returns></returns>
+        public int GetCommentCount(int PostID)
+        {
+            object Count = MySqlManager.GetKey(string.Format("SELECT COUNT(*) FROM {0} WHERE PostID = {1}", Tables.Comment, PostID));
+            return Count == DBNull.Value ? 0 : Convert.ToInt32(Count);
+        }
+
+        /// <summary>
+        /// 获得评论属性
+        /// </summary>
+        /// <typeparam name="T">属性类型</typeparam>
+        /// <param name="CommentID">目标评论ID</param>
+        /// <returns></returns>
+        public string GetComment<T>(int CommentID) where T : ICommentKey
+        {
+            /* int类型传入，SQL注入无效 */
+            string SQL = string.Format
+                (
+                "SELECT {0} FROM {1} WHERE CommentID = {2}"
+                , typeof(T).Name, Tables.Comment, CommentID
+                );
+            return MySqlManager.GetKey(SQL).ToString();
+        }
+
+        /// <summary>
+        /// 得到被评论文章的ID列表
+        /// </summary>
+        /// <returns></returns>
+        public List<int> GetCommentedPostID()
+        {
+            var List = new List<int>();
+
+            string SQL = string.Format("SELECT ID FROM {0} JOIN {1} ON {0}.ID={1}.PostID GROUP BY {0}.ID", Tables.Index, Tables.Comment);
+
+            foreach (DataRow Row in MySqlManager.GetTable(SQL).Rows)
+            {
+                List.Add(Convert.ToInt32(Row[0]));
+            }
+
+            return List;
+        }
+        /// <summary>
+        /// 获得目标文章的评论列表
+        /// </summary>
+        /// <param name="PostID">目标文章ID</param>
+        /// <returns></returns>
+        public CommentSet GetCommentList(int PostID)
+        {
+            CommentSet CommentSet = new CommentSet();
+
+            /* 按楼层排序 */
+            string SQL = string.Format("SELECT * FROM {0} WHERE PostID = ?PostID ORDER BY Floor", Tables.Comment);
+
+            List<MySqlParm> ParmList = new List<MySqlParm>
+                {
+                    new MySqlParm() { Name = "?PostID", Val = PostID }
+                };
+
+            using (MySqlCommand MySqlCommand = MySqlManager.ParmQueryCMD(SQL, ParmList))
+            {
+                DataTable result = MySqlManager.GetTable(MySqlCommand);
+
+                foreach (DataRow Row in result.Rows)
+                {
+                    CommentSet.Add(new Comment
+                    {
+                        CommentID = Convert.ToInt32(Row["CommentID"]),
+                        HEAD = Convert.ToInt32(Row["HEAD"]),
+                        PostID = Convert.ToInt32(Row["PostID"]),
+                        Floor = Convert.ToInt32(Row["Floor"]),
+
+                        User = Convert.ToString(Row["User"]),
+                        Email = Convert.ToString(Row["Email"]),
+                        Content = Convert.ToString(Row["Content"]),
+                        WebSite = Convert.ToString(Row["WebSite"]),
+                        Time = Convert.ToDateTime(Row["Time"]),
+                    });
+                }
+                return CommentSet;
+            }
+        }
+        /// <summary>
+        /// 获得目标评论的回复列表
+        /// </summary>
+        /// <param name="CommentID"></param>
+        /// <returns></returns>
+        public CommentSet GetCommentReplyList(int CommentID)
+        {
+            CommentSet CommentSet = new CommentSet();
+
+            DataTable result =
+                MySqlManager.GetTable(string.Format("SELECT * FROM {0} WHERE HEAD={1} ORDER BY Floor", Tables.Comment, CommentID));
+
+            foreach (DataRow Row in result.Rows)
+            {
+                CommentSet.Add(new Comment
+                {
+                    /* 数据库中的量 */
+                    CommentID = Convert.ToInt32(Row["CommentID"]),
+                    HEAD = Convert.ToInt32(Row["HEAD"]),
+                    PostID = Convert.ToInt32(Row["PostID"]),
+                    Floor = Convert.ToInt32(Row["Floor"]),
+
+                    User = Convert.ToString(Row["User"]),
+                    Email = Convert.ToString(Row["Email"]),
+                    Content = Convert.ToString(Row["Content"]),
+                    WebSite = Convert.ToString(Row["WebSite"]),
+                    Time = Convert.ToDateTime(Row["Time"]),
+                });
+            }
+            return CommentSet;
+        }
+
+        /// <summary>
+        /// 添加评论(CommentID和Time由系统生成，无需传入)
+        /// </summary>
+        /// <param name="Comment">评论内容</param>
+        /// <returns></returns>
+        public bool AddComment(Comment Comment)
+        {
+            string SQL = string.Format(
+                "INSERT INTO {0} " +
+                "(CommentID , HEAD, PostID, Floor, User, Email, Content, WebSite, Time) VALUES " +
+                "(?CommentID,?HEAD,?PostID,?Floor,?User,?Email,?Content,?WebSite,?Time)"
+
+                , Tables.Comment);
+
+            List<MySqlParm> ParmList = new List<MySqlParm>
+            {
+                new MySqlParm() { Name = "CommentID", Val = GetMaxCommentID() + 1 },
+                new MySqlParm() { Name = "HEAD", Val =  Comment.HEAD},
+                new MySqlParm() { Name = "PostID", Val =  Comment.PostID},
+                new MySqlParm() { Name = "Floor", Val =  GetMaxFloor(Comment.PostID) + 1 },
+
+                new MySqlParm() { Name = "User", Val =  Comment.User},
+                new MySqlParm() { Name = "Email", Val =  Comment.Email},
+                new MySqlParm() { Name = "Content", Val =  Comment.Content},
+                new MySqlParm() { Name = "WebSite", Val =  Comment.WebSite},
+                new MySqlParm() { Name = "Time", Val =  DateTime.Now},
+            };
+
+            MySqlCommand MySqlCommand = MySqlManager.ParmQueryCMD(SQL, ParmList);
+            MySqlCommand.Connection = MySqlManager.Connection;
+
+            /* 开始事务 */
+            MySqlCommand.Transaction = MySqlManager.Connection.BeginTransaction();
+
+            if (MySqlCommand.ExecuteNonQuery() == 1)
+            {
+                /* 指向表和拷贝表分别添加1行数据 */
+                MySqlCommand.Transaction.Commit();
+                return true;
+            }
+            else
+            {
+                MySqlCommand.Transaction.Rollback();
+                return false;
+            }
+        }
+        /// <summary>
+        /// 删除评论(相关回复不会被删除)
+        /// </summary>
+        /// <param name="CommentID"></param>
+        /// <returns></returns>
+        public bool DeleteComment(int CommentID)
+        {
+            MySqlCommand MySqlCommand = new MySqlCommand
+            {
+                CommandText = string.Format("DELETE FROM {0} WHERE CommentID = {1}", Tables.Comment, CommentID),
+                Connection = MySqlManager.Connection,
+
+                /* 开始事务 */
+                Transaction = MySqlManager.Connection.BeginTransaction()
+            };
+
+            if (MySqlManager.QueryOnly(ref MySqlCommand) == 1)
+            {
+                /* 删除1条评论，操作行为1 */
+                MySqlCommand.Transaction.Commit();
+                return true;
+            }
+            else
+            {
+                MySqlCommand.Transaction.Rollback();
+                return false;
+            }
+        }
+    }
+}
