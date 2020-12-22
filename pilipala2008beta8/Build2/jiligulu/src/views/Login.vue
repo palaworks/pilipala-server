c31ef8e371dc7494190074cce8090867634973a8
Thaumy Cheng <Studio_ai@outlook.com>
2020-12-22 23:32:32 +0800

    add user model

---
+++ pilipala2008beta8/Build2/jiligulu/src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <v-card style="margin:auto;width:50%;" :loading="this.login_data.is_loading">
+      <v-card-title>
+        <div style="font-size:24px;margin-top:10px" class="mx-auto">欢迎来到叽里咕噜</div>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field v-model="login_data.UserName" label="用户名" hint="UserName" auto-grow></v-text-field>
+        <v-text-field
+          v-model="login_data.UserPWD"
+          label="密码"
+          hint="UserPWD"
+          type="password"
+          auto-grow
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          class="mx-auto"
+          style="margin-bottom:10px;"
+          width="300px"
+          color="primary"
+          @click="Login()"
+        >Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</template>
+<script>
+import qs from "qs";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      login_data: {
+        UserName: "",
+        UserPWD: "",
+        is_loading: false,
+      },
+    };
+  },
+  methods: {
+    Login: function () {
+      this.is_loading = true;
+      this.$axios({
+        method: "post",
+        url: this.glob.root_path + "/user/Login",
+        data: qs.stringify({
+          UserName: this.login_data.UserName,
+          UserPWD: this.login_data.UserPWD,
+        }),
+      }).then((response) => {
+        if (response.data == true) {
+          this.is_loading = false;
+          this.$root.navi_show = true;
+          this.$root.UserName = this.login_data.UserName;
+          this.$router.push({ name: "PostList" });
+        }
+      });
+    },
+  },
+};
+</script>
