ae270b0ad566c21884ca1c11e0f1508acb6d27d0
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-07 18:19:05 +0800

    beta9-preview init

---
+++ pilipala2008beta9/PILIPALA/PILIPALA/Models/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PILIPALA.Models
+{
+    public class AppSettings
+    {
+    }
+    public class User
+    {
+        public string Account { get; set; }
+        public string PWD { get; set; }
+    }
+}
