3371a6ed9a439db62bbb15ab94a798455768f2b5
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-10 12:24:11 +0800

    ~~

---
+++ pilipala2008beta9/jiligulu/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-card class="d-flex flex-row">
+      <Navi v-if="this.$root.navi_show" />
+      <v-card :class="this.$root.navi_show?'ml-14':''" width="100%">
+    
+          <router-view class="px-2" />
+        
+      </v-card>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+import Navi from "./components/Navi";
+
+export default {
+  name: "App",
+
+  components: {
+    Navi,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
