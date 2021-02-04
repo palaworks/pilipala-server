3c4f05f9119d976fd2eb0cbd89dd13cbb86bcf32
Thaumy Cheng <Studio_ai@outlook.com>
2021-02-05 00:19:44 +0800

    clean

---
+++ pilipala2008beta9/PILIPALA/PILIPALA/Controllers/PanelController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using PILIPALA.system.Theme;
+using WaterLibrary.Utils;
+using WaterLibrary.pilipala;
+using WaterLibrary.pilipala.Entity;
+using WaterLibrary.pilipala.Entity.PostProp;
+using WaterLibrary.pilipala.Components;
+
+namespace PILIPALA.Controllers
+{
+    public class PanelController : Controller
+    {
+        private readonly Reader Reader;
+        private readonly Writer Writer;
+        private readonly Counter Counter;
+        private readonly CommentLake CommentLake;
+        private readonly ThemeHandler ThemeHandler;
+        private readonly ComponentFactory ComponentFactory = new();
+
+        public PanelController(ICORE CORE, ThemeHandler ThemeHandler)
+        {
+            CORE.CoreReady += ComponentFactory.Ready;
+
+            /* 启动内核 */
+            CORE.Run();
+
+            Reader = ComponentFactory.GenReader(Reader.ReadMode.CleanRead);
+            Writer = ComponentFactory.GenWriter();
+            Counter = ComponentFactory.GenCounter();
+            CommentLake = ComponentFactory.GenCommentLake();
+            this.ThemeHandler = ThemeHandler;
+        }
+
+        public ActionResult List(bool ajax)
+        {
+            string REGEXP(string s)
+            {
+                /* 读取主题配置文件并取得要在本页展示的文章归档 */
+                var archive = ThemeHandler.Config["Pannel"][s].ToList();
+                return ConvertH.ListToString(archive, '|');
+            }
+
+            PostSet PostSet置顶 = new PostSet();
+            foreach (Post el in Reader.GetPost<ArchiveName>(REGEXP("ToppedArchive")))
+            {
+                el.PropertyContainer.Add("CommentCount", CommentLake.GetCommentCount(el.PostID));
+                PostSet置顶.Add(el);
+            }
+            ViewBag.置顶文章 = PostSet置顶;
+
+            PostSet PostSet其他 = new PostSet();
+            foreach (Post el in Reader.GetPost<ArchiveName>(REGEXP("DefaultArchive")))
+            {
+                el.PropertyContainer.Add("CommentCount", CommentLake.GetCommentCount(el.PostID));
+                PostSet其他.Add(el);
+            }
+            ViewBag.其他文章 = PostSet其他;
+
+            if (ajax == false)
+            {
+                ViewBag.Layout = "~/Views/Shared/_Layout.cshtml";
+                return View();
+            }
+            else
+            {
+                ViewBag.Layout = null;
+                return View();
+            }
+
+        }
+        public ActionResult Content(int ID, bool ajax)
+        {
+            string REGEXP(string s = "DefaultArchive")
+            {
+                /* 读取主题配置文件并取得要在本页展示的文章归档 */
+                var archive = ThemeHandler.Config["Pannel"][s].ToList();
+                return ConvertH.ListToString(archive, '|');
+            }
+
+            ViewBag.ID = ID;//请求ID
+
+            ViewBag.Post = Reader.GetPost(ID);//文章数据
+            ViewBag.Post.PropertyContainer.Add("CommentCount", CommentLake.GetCommentCount(ID));//添加评论计数，可优化
+
+
+            ViewBag.CommentList = CommentLake.GetComments(ID);//评论数据
+
+            ViewBag.PrevID = Reader.Smaller<PostID>(ID, REGEXP(), PostPropEnum.ArchiveName);
+            ViewBag.PrevTitle = Reader.GetPostProp<Title>(ViewBag.PrevID);
+
+            ViewBag.NextID = Reader.Bigger<PostID>(ID, REGEXP(), PostPropEnum.ArchiveName);
+            ViewBag.NextTitle = Reader.GetPostProp<Title>(ViewBag.NextID);
+
+
+
+            if (ajax == false)
+            {
+                ViewBag.Layout = "~/Views/Shared/_Layout.cshtml";
+                return View();
+            }
+            else
+            {
+                ViewBag.Layout = null;
+                return View();
+            }
+
+        }
+    }
+}
