83ae09fc0cc2d2882003b52fd0133d5980d7649c
Thaumy Cheng <Studio_ai@outlook.com>
2021-02-11 23:03:11 +0800

    ~~

--- pilipala2008beta9/LightningLink/LightningLink/LightningLink.cs
+++ pilipala2008beta9/LightningLink/LightningLink/LightningLink.cs
@@ -1,8 +1,36 @@
 ﻿using System;
 
+using WaterLibrary.MySQL;
+
+
 namespace WaterLibrary.pilipala.Component
 {
     public class LightningLink
     {
+        /// <summary>
+        /// 私有构造
+        /// </summary>
+        private LightningLink() { }
+        /// <summary>
+        /// 工厂构造
+        /// </summary>
+        /// <param name="Table"></param>
+        /// <param name="MySqlManager"></param>
+        internal LightningLink(string Table, MySqlManager MySqlManager)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 扩展方法
+    /// </summary>
+    public static class Extension
+    {
+        /// <summary>
+        /// 生成评论湖组件
+        /// </summary>
+        /// <returns></returns>
+        public static LightningLink GenLightningLink(this ComponentFactory src) => new(src.PluginTables["plugin__lightning_link"], src.MySqlManager);
     }
 }
