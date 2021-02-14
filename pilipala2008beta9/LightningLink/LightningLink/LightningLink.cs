9a63688b09385c160dde868c6b180f622fb422d9
Thaumy Cheng <Studio_ai@outlook.com>
2021-02-14 19:00:15 +0800

    ~~

--- pilipala2008beta9/LightningLink/LightningLink/LightningLink.cs
+++ pilipala2008beta9/LightningLink/LightningLink/LightningLink.cs
@@ -7,6 +7,9 @@ namespace WaterLibrary.pilipala.Component
 {
     public class LightningLink
     {
+        private string Table { get; init; }
+        private MySqlManager MySqlManager { get; init; }
+
         /// <summary>
         /// 私有构造
         /// </summary>
@@ -18,6 +21,25 @@ namespace WaterLibrary.pilipala.Component
         /// <param name="MySqlManager"></param>
         internal LightningLink(string Table, MySqlManager MySqlManager)
         {
+            this.Table = Table;
+            this.MySqlManager = MySqlManager;
+        }
+
+        /// <summary>
+        /// 新建链接
+        /// </summary>
+        /// <param name="Identifier">标记，即<{mark}>的mark文本</param>
+        /// <param name="Text">文本，即被标记替换的内容</param>
+        public void NewLink(string Identifier, string Text)
+        {
+
+        }
+        /// <summary>
+        /// 删除链接
+        /// </summary>
+        /// <param name="Identifier">标记</param>
+        public void DelLink(string Identifier)
+        {
 
         }
     }
@@ -25,12 +47,12 @@ namespace WaterLibrary.pilipala.Component
     /// <summary>
     /// 扩展方法
     /// </summary>
-    public static class Extension
+    public static class ComponentExtensions
     {
         /// <summary>
         /// 生成评论湖组件
         /// </summary>
         /// <returns></returns>
-        public static LightningLink GenLightningLink(this ComponentFactory src) => new(src.PluginTables["plugin__lightning_link"], src.MySqlManager);
+        public static LightningLink GenLightningLink(this ComponentFactory src) => new(CORE.TableCache["plugin__lightning_link"], CORE.MySqlManager);
     }
 }
