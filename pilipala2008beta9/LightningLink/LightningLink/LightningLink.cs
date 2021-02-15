8f7def3a4a3ac4c29330d3293637424b0e203f7f
Thaumy Cheng <Studio_ai@outlook.com>
2021-02-15 11:09:55 +0800

    ~~

--- pilipala2008beta9/LightningLink/LightningLink/LightningLink.cs
+++ pilipala2008beta9/LightningLink/LightningLink/LightningLink.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 
 using WaterLibrary.MySQL;
 
@@ -7,8 +9,19 @@ namespace WaterLibrary.pilipala.Component
 {
     public class LightningLink
     {
-        private string Table { get; init; }
+        private string LinkTable { get; init; }
         private MySqlManager MySqlManager { get; init; }
+        private Dictionary<string, string> LinkCache { get; set; }
+        private void RefreshCache()
+        {
+            LinkCache.Clear();
+            //重建缓存
+            foreach (DataRow Row in MySqlManager.GetTable($"SELECT * FROM {LinkTable}").Rows)
+            {
+                string LinkName = "<{" + Row["LinkName"].ToString() + "}>";
+                LinkCache.Add(LinkName, Row["LinkText"].ToString());
+            }
+        }
 
         /// <summary>
         /// 私有构造
@@ -19,28 +32,48 @@ namespace WaterLibrary.pilipala.Component
         /// </summary>
         /// <param name="Table"></param>
         /// <param name="MySqlManager"></param>
-        internal LightningLink(string Table, MySqlManager MySqlManager)
+        internal LightningLink(string LinkTable, MySqlManager MySqlManager)
         {
-            this.Table = Table;
+            this.LinkTable = LinkTable;
             this.MySqlManager = MySqlManager;
+
+            RefreshCache();
         }
 
         /// <summary>
         /// 新建链接
         /// </summary>
-        /// <param name="Identifier">标记，即<{mark}>的mark文本</param>
+        /// <param name="LinkName">标记，即<{mark}>的mark文本</param>
         /// <param name="Text">文本，即被标记替换的内容</param>
-        public void NewLink(string Identifier, string Text)
+        public void NewLink(string LinkName, string LinkText)
         {
-
+            LinkName = "<{" + LinkName + "}>";
+            MySqlManager.ExecuteInsert(LinkTable, new("LinkName", LinkName), new("LinkText", LinkText));
+            LinkCache.Add(LinkName, LinkText);
         }
         /// <summary>
         /// 删除链接
         /// </summary>
-        /// <param name="Identifier">标记</param>
-        public void DelLink(string Identifier)
+        /// <param name="LinkName">标记</param>
+        public void DelLink(string LinkName)
         {
+            LinkName = "<{" + LinkName + "}>";
+            MySqlManager.ExecuteDelete(LinkTable, ("LinkName", LinkName));
+            LinkCache.Remove(LinkName);
+        }
 
+        /// <summary>
+        /// 应用链接
+        /// </summary>
+        /// <param name="Text">含有链接的文本</param>
+        /// <returns></returns>
+        public string ApplyLink(string Text)
+        {
+            foreach (var el in LinkCache)
+            {
+                Text = Text.Replace(el.Key, el.Value);
+            }
+            return Text;
         }
     }
 
