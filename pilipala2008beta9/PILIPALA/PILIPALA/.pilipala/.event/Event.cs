f43dfb52ff36d8f646dfe06fe7a90f876a9c3fe2
Thaumy Cheng <Studio_ai@outlook.com>
2021-02-22 10:24:45 +0800

    ~~

--- pilipala2008beta9/PILIPALA/PILIPALA/.pilipala/.event/Event.cs
+++ pilipala2008beta9/PILIPALA/PILIPALA/.pilipala/.event/Event.cs
@@ -8,47 +8,53 @@ namespace PILIPALA.Event
     public class Event
     {
         public delegate void BeforeEvent();
-        public static void CoreReadyBeforeEventHandler()
+
+        public class Before
         {
-            CoreReadyBefore += () => { };
-            CoreReadyBefore();
+            public static void GetPostEventHandler()
+            {
+                GetPost += () => { };
+                GetPost();
+            }
+
+            public static event BeforeEvent DocHead;
+            public event BeforeEvent DocBody;
+            public event BeforeEvent DocFoot;
+            public event BeforeEvent DocComplete;
+
+            public static event BeforeEvent GetPost;
+            public static event BeforeEvent CreatePost;
+            public event BeforeEvent DeletePost;
+
+            public event BeforeEvent CreateComment;
+            public event BeforeEvent DeleteComment;
         }
-        public static event BeforeEvent CoreReadyBefore;
 
-        public event BeforeEvent DocHeadBefore;
-        public event BeforeEvent Doc_body_before;
-        public event BeforeEvent Doc_foot_before;
-        public event BeforeEvent Doc_all_before;
-
-        public event BeforeEvent Post_load_before;
-        public event BeforeEvent Post_create_before;
-        public event BeforeEvent Post_delete_before;
-        public event BeforeEvent PostContent_get_before;
-
-        public event BeforeEvent Comment_create_before;
-        public event BeforeEvent Comment_delete_delete;
 
 
 
         public delegate void AfterEvent();
-        public static void CoreReadyAfterEventHandler()
+
+
+        public class After
         {
-            CoreReadyAfter += () => { };
-            CoreReadyAfter();
+            public static void GetPostEventHandler()
+            {
+                GetPost += () => { };
+                GetPost();
+            }
+
+            public event AfterEvent DocHead;
+            public event AfterEvent DocBody;
+            public event AfterEvent DocFoot;
+            public event AfterEvent DocComplete;
+
+            public static event AfterEvent GetPost;
+            public event AfterEvent CreatePost;
+            public event AfterEvent DeletePost;
+
+            public event AfterEvent CreateComment;
+            public event AfterEvent DeleteComment;
         }
-        public static event AfterEvent CoreReadyAfter;
-
-        public event AfterEvent Doc_head_after;
-        public event AfterEvent Doc_body_after;
-        public event AfterEvent Doc_foot_after;
-        public event AfterEvent Doc_all_after;
-
-        public event AfterEvent Post_load_after;
-        public event AfterEvent Post_create_after;
-        public event AfterEvent Post_delete_after;
-        public event AfterEvent PostContent_get_after;
-
-        public event AfterEvent Comment_create_after;
-        public event AfterEvent Comment_delete_after;
     }
 }
