c1b6fbc407a3bb9259dbc0d1e743fcb9effc5740
Thaumy <Studio_ai@outlook.com>
2021-05-31 17:38:55 +0800

    ~~

---
+++ pilipala2008beta9/PILIPALA/PILIPALA/.pilipala/.plugin/Plugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PILIPALA.pilipala.plugin
+{
+    public class Plugin
+    {
+    }
+}
