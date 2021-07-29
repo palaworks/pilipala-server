81edb5e666da65437f7c8efafc62a7074d7bc5a8
Thaumy <Studio_ai@outlook.com>
2021-07-29 15:50:16 +0800

    ~~

--- pilipala2008beta9/PILIPALA/PILIPALA/obj/Release/net5.0/PubTmp/Out/wwwroot/ui/js/more/note.js
+++
@@ -1,7 +0,0 @@
-﻿let typed = new Typed('#note', {
-    strings: ['你好！~~^400', '这里是THAUMY的博客！^300','又一个码农的家', '又一个码农的家.', '又一个码农的家..', '又一个码农的家...'],
-    typeSpeed: 100,
-    backSpeed: 0,
-    smartBackspace: true,
-    loop: false
-});
