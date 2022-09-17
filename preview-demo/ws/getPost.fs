97cb68cd894c7d418ee9b466e1110065bd462a59
Thaumy <thaumy@outlook.com>
2022-09-17 22:58:15 +0800

    Update getPost.fs

--- preview-demo/ws/getPost.fs
+++ preview-demo/ws/getPost.fs
@@ -2,6 +2,10 @@ module ws.getPost
 
 open System
 open app.user
+open fsharper.op
+open fsharper.typ
+open fsharper.op.Foldable
+open pilipala.container.comment
 open pilipala.util.text
 open WebSocketSharp.Server
 
@@ -11,38 +15,69 @@ type getPost() =
     override self.OnMessage e =
         Console.WriteLine $"getPost from client:{e.Data}"
 
-        let x =
+        let post =
             user.GetPost(Int64.Parse e.Data).unwrap ()
+
+        let comments =
+            post.Comments.unwrap().foldr
+            <| fun (comment: Comment) acc ->
+                let json =
+                    $"""{{
+                        "Id":{comment.Id},
+                        "User":"{comment.UserId.unwrap ()}",
+                        "Body":{comment.Body.unwrap().serializeToJson().json},
+                        "ReplyTo":{post.Id},
+                        "SiteUrl":"https://www.thaumy.cn",
+                        "AvatarUrl":"/src/assets/comment_user_avatars/kurumi.jpg",
+                        "CreateTime":"{comment.CreateTime.unwrap().ToIso8601()}"
+                    }}"""
+
+                $",{json}{acc}"
+            <| ""
+            |> fun s ->
+                $"[{if s.Length <> 0 then
+                        s.Substring(1)
+                    else
+                        String.Empty}]"
 
         let json =
             $"""{{
-                "Id": {x.Id},
-                "Title":"{x.Title.unwrap ()}",
-                "Body":{x.Body.unwrap().serializeToJson().json},
-                "CreateTime":"{x.CreateTime.unwrap ()}",
-                "ModifyTime":"{x.ModifyTime.unwrap ()}",
-                "CoverUrl":{x.["CoverUrl"].unwrap().unwrapOr (fun _ -> "null")},
-                "Summary":{x.["CoverUrl"].unwrap().unwrapOr (fun _ -> "null")},
-                "IsGeneratedSummary":{x.["IsGeneratedSummary"]
+                        "Id": {post.Id},
+                        "Title":"{post.Title.unwrap ()}",
+                        "Body":{post.Body.unwrap().serializeToJson().json},
+                        "CreateTime":"{post.CreateTime.unwrap().ToIso8601()}",
+                        "ModifyTime":"{post.ModifyTime.unwrap().ToIso8601()}",
+                        "CoverUrl":{post.["CoverUrl"]
+                                        .unwrap()
+                                        .unwrap()
+                                        .coerce<Option'<string>>()
+                                        .fmap(fun s -> $"\"{s}\"")
+                                        .unwrapOr (fun _ -> "null")},
+                        "Summary":{post.["Summary"]
+                                       .unwrap()
+                                       .fmap(fun s -> s.serializeToJson().json)
+                                       .unwrapOr (fun _ -> "null")},
+                        "IsGeneratedSummary":{post.["IsGeneratedSummary"]
+                                                  .unwrap()
+                                                  .fmap(fun b -> b.ToString().ToLower())
+                                                  .unwrapOr (fun _ -> "false")},
+                        "ViewCount":{post.["ViewCount"]
+                                         .unwrap()
+                                         .unwrapOr (fun _ -> "0")},
+                        "Comments":{comments},
+                        "CanComment":{post.CanComment.ToString().ToLower()},
+                        "IsArchive":{post.["IsArchive"]
+                                         .unwrap()
+                                         .fmap(fun b -> b.ToString().ToLower())
+                                         .unwrapOr (fun _ -> "false")},
+                        "IsSchedule":{post.["IsSchedule"]
                                           .unwrap()
                                           .fmap(fun b -> b.ToString().ToLower())
                                           .unwrapOr (fun _ -> "false")},
-                "ViewCount":{x.["CoverUrl"].unwrap().unwrapOr (fun _ -> "0")},
-                "Comments":[],
-                "CanComment":{x.CanComment.ToString().ToLower()},
-                "IsArchive":{x.["CoverUrl"]
-                                 .unwrap()
-                                 .fmap(fun x -> x.ToString().ToLower())
-                                 .unwrapOr (fun _ -> "false")},
-                "IsSchedule":{x.["CoverUrl"]
-                                  .unwrap()
-                                  .fmap(fun x -> x.ToString().ToLower())
-                                  .unwrapOr (fun _ -> "false")},
-                "Topics":{x.["CoverUrl"]
-                              .unwrap()
-                              .fmap(fun x -> x.ToString().ToLower())
-                              .unwrapOr (fun _ -> "false")}
-            }}"""
+                        "Topics":{post.["Topics"].unwrap().unwrapOr(fun _ -> [||])
+                                      .serializeToJson()
+                                      .json}
+                    }}"""
 
         Console.WriteLine(json)
         self.Send(json)
