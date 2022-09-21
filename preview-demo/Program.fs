db405ae62cee1c70e1da25c0bbe77e15d3823f06
Thaumy <thaumy@outlook.com>
2022-09-21 19:15:56 +0800

    added view count

--- preview-demo/Program.fs
+++ preview-demo/Program.fs
@@ -2,9 +2,11 @@
 open ws.server
 open ws.getPost
 open ws.getAllPost
+open ws.getAllPostId
 
 wsServer.AddWebSocketService<getPost>("/get_post")
 wsServer.AddWebSocketService<getAllPost>("/get_all_post")
+wsServer.AddWebSocketService<getAllPostId>("/get_all_post_id")
 
 wsServer.Start()
 Console.ReadKey() |> ignore
