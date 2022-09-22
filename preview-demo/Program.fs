f8803bdda768688316f577b4922b8e0183861f17
Thaumy <thaumy@outlook.com>
2022-09-22 11:52:39 +0800

    test PartialOrder plugin

--- preview-demo/Program.fs
+++ preview-demo/Program.fs
@@ -3,10 +3,14 @@ open ws.server
 open ws.getPost
 open ws.getAllPost
 open ws.getAllPostId
+open ws.getPrevPost
+open ws.getNextPost
 
 wsServer.AddWebSocketService<getPost>("/get_post")
 wsServer.AddWebSocketService<getAllPost>("/get_all_post")
 wsServer.AddWebSocketService<getAllPostId>("/get_all_post_id")
+wsServer.AddWebSocketService<getPrevPost>("/get_prev_post")
+wsServer.AddWebSocketService<getNextPost>("/get_next_post")
 
 wsServer.Start()
 Console.ReadKey() |> ignore
--- preview-demo/ws/getPost.fs
+++ preview-demo/ws/getPost.fs
@@ -5,18 +5,14 @@ open app.user
 open fsharper.op
 open fsharper.typ
 open fsharper.op.Foldable
+open pilipala.container.post
 open pilipala.container.comment
 open pilipala.util.text
 open WebSocketSharp.Server
 
-type getPost() =
-    inherit WebSocketBehavior()
 
-    override self.OnMessage e =
-        Console.WriteLine $"getPost from client:{e.Data}"
-
-        let post =
-            user.GetPost(Int64.Parse e.Data).unwrap ()
+type Post with
+    member post.encodeToJson() =
 
         let rec getRecursiveComments (comment: Comment) =
             match comment.Comments.unwrap().toList () with
@@ -52,43 +48,53 @@ type getPost() =
                     else
                         String.Empty}]"
 
-        let json =
-            $"""{{
-                        "Id": {post.Id},
-                        "Title":"{post.Title.unwrap ()}",
-                        "Body":{post.Body.unwrap().serializeToJson().json},
-                        "CreateTime":"{post.CreateTime.unwrap().ToIso8601()}",
-                        "ModifyTime":"{post.ModifyTime.unwrap().ToIso8601()}",
-                        "CoverUrl":{post.["CoverUrl"]
-                                        .unwrap()
-                                        .unwrap()
-                                        .cast<Option'<string>>()
-                                        .fmap(fun s -> $"\"{s}\"")
-                                        .unwrapOr (fun _ -> "null")},
-                        "Summary":{post.["Summary"]
+        $"""{{
+                    "Id": {post.Id},
+                    "Title":"{post.Title.unwrap ()}",
+                    "Body":{post.Body.unwrap().serializeToJson().json},
+                    "CreateTime":"{post.CreateTime.unwrap().ToIso8601()}",
+                    "ModifyTime":"{post.ModifyTime.unwrap().ToIso8601()}",
+                    "CoverUrl":{post.["CoverUrl"]
+                                    .unwrap()
+                                    .unwrap()
+                                    .cast<Option'<string>>()
+                                    .fmap(fun s -> $"\"{s}\"")
+                                    .unwrapOr (fun _ -> "null")},
+                    "Summary":{post.["Summary"]
+                                   .unwrap()
+                                   .fmap(fun s -> s.serializeToJson().json)
+                                   .unwrapOr (fun _ -> "null")},
+                    "IsGeneratedSummary":{post.["IsGeneratedSummary"]
+                                              .unwrap()
+                                              .fmap(fun b -> b.ToString().ToLower())
+                                              .unwrapOr (fun _ -> "false")},
+                    "ViewCount":{post.["ViewCount"]
+                                     .unwrap()
+                                     .unwrapOr (fun _ -> "0")},
+                    "Comments":{comments_json},
+                    "CanComment":{post.CanComment.ToString().ToLower()},
+                    "IsArchived":{post.["IsArchived"]
+                                      .unwrap()
+                                      .fmap(fun b -> b.ToString().ToLower())
+                                      .unwrapOr (fun _ -> "false")},
+                    "IsScheduled":{post.["IsScheduled"]
                                        .unwrap()
-                                       .fmap(fun s -> s.serializeToJson().json)
-                                       .unwrapOr (fun _ -> "null")},
-                        "IsGeneratedSummary":{post.["IsGeneratedSummary"]
-                                                  .unwrap()
-                                                  .fmap(fun b -> b.ToString().ToLower())
-                                                  .unwrapOr (fun _ -> "false")},
-                        "ViewCount":{post.["ViewCount"]
-                                         .unwrap()
-                                         .unwrapOr (fun _ -> "0")},
-                        "Comments":{comments_json},
-                        "CanComment":{post.CanComment.ToString().ToLower()},
-                        "IsArchived":{post.["IsArchived"]
-                                          .unwrap()
-                                          .fmap(fun b -> b.ToString().ToLower())
-                                          .unwrapOr (fun _ -> "false")},
-                        "IsScheduled":{post.["IsScheduled"]
-                                           .unwrap()
-                                           .fmap(fun b -> b.ToString().ToLower())
-                                           .unwrapOr (fun _ -> "false")},
-                        "Topics":{post.["Topics"].unwrap().unwrapOr(fun _ -> [||])
-                                      .serializeToJson()
-                                      .json}
-                    }}"""
+                                       .fmap(fun b -> b.ToString().ToLower())
+                                       .unwrapOr (fun _ -> "false")},
+                    "Topics":{post.["Topics"].unwrap().unwrapOr(fun _ -> [||])
+                                  .serializeToJson()
+                                  .json}
+                }}"""
+
+type getPost() =
+    inherit WebSocketBehavior()
+
+    override self.OnMessage e =
+        Console.WriteLine $"getPost from client:{e.Data}"
+
+        let post =
+            user.GetPost(Int64.Parse e.Data).unwrap ()
+
+        let json = post.encodeToJson ()
 
         self.Send(json)
