ba71a2750b82a4dc6ba59ab2018b3c33f46449f0
Thaumy <thaumy@outlook.com>
2022-09-25 00:42:26 +0800

    new json serialization

--- preview-demo/ws/getPost.fs
+++ preview-demo/ws/getPost.fs
@@ -11,6 +11,32 @@ open pilipala.container.comment
 open pilipala.util.text
 open WebSocketSharp.Server
 
+type CommentJson =
+    { Id: i64
+      User: string
+      Body: string
+      ReplyTo: i64
+      SiteUrl: string
+      AvatarUrl: string
+      CreateTime: string }
+
+type PostJson =
+    { Id: i64
+      Title: string
+      Body: string
+      CreateTime: string
+      ModifyTime: string
+      CoverUrl: string
+      Summary: string
+      IsGeneratedSummary: bool
+      ViewCount: u32
+      Comments: CommentJson array
+      CanComment: bool
+      IsArchived: bool
+      IsScheduled: bool
+      Topics: string array
+      PrevId: i64
+      NextId: i64 }
 
 type Post with
     member post.encodeToJson() =
@@ -23,91 +49,93 @@ type Post with
                 <| fun acc c -> acc @ (comment.Id, c) :: getRecursiveComments c
                 <| []
 
-        let comments_json =
+        let comments =
             (post.Comments.unwrap().foldl
              <| fun acc c -> acc @ (post.Id, c) :: getRecursiveComments c
              <| []
              |> List.sortBy (fun x -> x.snd().CreateTime |> unwrap))
                 .foldr
             <| fun (replyTo, comment: Comment) acc ->
-                let json =
-                    $"""{{
-                        "Id":{comment.Id},
-                        "User":"{comment.UserName.unwrapOr (fun _ -> comment.UserId.ToString())}",
-                        "Body":{comment.Body.unwrap().serializeToJson().json},
-                        "ReplyTo":{replyTo},
-                        "SiteUrl":{comment.["UserSiteUrl"]
-                                       .unwrap()
-                                       .unwrap()
-                                       .cast<Option'<string>>()
-                                       .fmap(fun url -> url.serializeToJson().json)
-                                       .unwrapOr (fun _ -> "null")},
-                        "AvatarUrl":{comment.["UserAvatarUrl"]
-                                         .unwrap()
-                                         .unwrap()
-                                         .cast<Option'<string>>()
-                                         .fmap(fun url -> url.serializeToJson().json)
-                                         .unwrapOr (fun _ -> "null")},
-                        "CreateTime":"{comment.CreateTime.unwrap().ToIso8601()}"
-                    }}"""
+                { Id = comment.Id
+                  User = comment.UserName.unwrapOr (fun _ -> comment.UserId.ToString())
+                  Body = comment.Body.unwrap ()
+                  ReplyTo = replyTo
+                  SiteUrl =
+                    comment.["UserSiteUrl"]
+                        .unwrap()
+                        .unwrap()
+                        .cast<Option'<string>>()
+                        .unwrapOr (fun _ -> null)
+                  AvatarUrl =
+                    comment.["UserAvatarUrl"]
+                        .unwrap()
+                        .unwrap()
+                        .cast<Option'<string>>()
+                        .unwrapOr (fun _ -> null)
+                  CreateTime = comment.CreateTime.unwrap().ToIso8601() }
+                :: acc
+            <| []
 
-                $",{json}{acc}"
-            <| ""
-            |> fun s ->
-                $"[{if s.Length <> 0 then
-                        s.Substring(1)
-                    else
-                        String.Empty}]"
+        { Id = post.Id
+          Title = post.Title.unwrap ()
+          Body = post.Body.unwrap ()
+          CreateTime = post.CreateTime.unwrap().ToIso8601()
+          ModifyTime = post.ModifyTime.unwrap().ToIso8601()
+          CoverUrl =
+            post.["CoverUrl"]
+                .unwrap()
+                .unwrap()
+                .cast<Option'<string>>()
+                .unwrapOr (fun _ -> null)
+          Summary =
+            post.["Summary"]
+                .unwrap()
+                .fmap(cast)
+                .unwrapOr (fun _ -> null)
+          IsGeneratedSummary =
+            post.["IsGeneratedSummary"]
+                .unwrap()
+                .fmap(cast)
+                .unwrapOr (fun _ -> false)
+          ViewCount =
+            post.["ViewCount"]
+                .unwrap()
+                .fmap(cast)
+                .unwrapOr (fun _ -> 0u)
+          Comments = comments.toArray ()
+          CanComment = post.CanComment
+          IsArchived =
+            post.["IsArchived"]
+                .unwrap()
+                .fmap(cast)
+                .unwrapOr (fun _ -> false)
+          IsScheduled =
+            post.["IsScheduled"]
+                .unwrap()
+                .fmap(cast)
+                .unwrapOr (fun _ -> false)
+          Topics =
+            post.["Topics"]
+                .unwrap()
+                .fmap(cast)
+                .unwrapOr (fun _ -> [||])
+          PrevId =
+            post.["PredId"]
+                .unwrap()
+                .unwrap()
+                .cast<Option'<obj>>()
+                .fmap(fun id -> id.cast<i64> ())
+                .unwrapOr (fun _ -> -1)
+          NextId =
+            post.["SuccId"]
+                .unwrap()
+                .unwrap()
+                .cast<Option'<obj>>()
+                .fmap(fun id -> id.cast<i64> ())
+                .unwrapOr (fun _ -> -1) }
+            .serializeToJson()
+            .json
 
-        $"""{{
-                    "Id": {post.Id},
-                    "Title":"{post.Title.unwrap ()}",
-                    "Body":{post.Body.unwrap().serializeToJson().json},
-                    "CreateTime":"{post.CreateTime.unwrap().ToIso8601()}",
-                    "ModifyTime":"{post.ModifyTime.unwrap().ToIso8601()}",
-                    "CoverUrl":{post.["CoverUrl"]
-                                    .unwrap()
-                                    .unwrap()
-                                    .cast<Option'<string>>()
-                                    .fmap(fun url -> url.serializeToJson().json)
-                                    .unwrapOr (fun _ -> "null")},
-                    "Summary":{post.["Summary"]
-                                   .unwrap()
-                                   .fmap(fun s -> s.serializeToJson().json)
-                                   .unwrapOr (fun _ -> "null")},
-                    "IsGeneratedSummary":{post.["IsGeneratedSummary"]
-                                              .unwrap()
-                                              .fmap(fun b -> b.ToString().ToLower())
-                                              .unwrapOr (fun _ -> "false")},
-                    "ViewCount":{post.["ViewCount"]
-                                     .unwrap()
-                                     .unwrapOr (fun _ -> "0")},
-                    "Comments":{comments_json},
-                    "CanComment":{post.CanComment.ToString().ToLower()},
-                    "IsArchived":{post.["IsArchived"]
-                                      .unwrap()
-                                      .fmap(fun b -> b.ToString().ToLower())
-                                      .unwrapOr (fun _ -> "false")},
-                    "IsScheduled":{post.["IsScheduled"]
-                                       .unwrap()
-                                       .fmap(fun b -> b.ToString().ToLower())
-                                       .unwrapOr (fun _ -> "false")},
-                    "Topics":{post.["Topics"].unwrap().unwrapOr(fun _ -> [||])
-                                  .serializeToJson()
-                                  .json},
-                    "PrevId":{post.["PredId"]
-                                  .unwrap()
-                                  .unwrap()
-                                  .cast<Option'<obj>>()
-                                  .fmap(fun id -> id.cast<i64>().ToString())
-                                  .unwrapOr (fun _ -> "null")},
-                    "NextId":{post.["SuccId"]
-                                  .unwrap()
-                                  .unwrap()
-                                  .cast<Option'<obj>>()
-                                  .fmap(fun id -> id.cast<i64>().ToString())
-                                  .unwrapOr (fun _ -> "null")}
-                }}"""
 
 type getPost() =
     inherit WebSocketBehavior()
