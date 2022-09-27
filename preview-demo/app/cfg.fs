b6c8882f059a2dda2504de344e8da8a350d47b94
Thaumy <thaumy@outlook.com>
2022-09-27 09:18:30 +0800

    test for deploy

--- preview-demo/app/cfg.fs
+++ preview-demo/app/cfg.fs
@@ -5,16 +5,28 @@ open pilipala.builder
 open pilipala.container.post
 open pilipala.container.comment
 open pilipala.plugin
+open pilipala.util.io
 open pilipala.util.hash
 open pilipala.util.text
+
+type Cfg =
+    { user: string
+      pwd: string
+      db_name: string
+      db_user: string
+      db_pwd: string }
+
+let cfg =
+    { json = readFile "./config/config.json" }
+        .deserializeTo<Cfg> ()
 
 let getDbCfg () =
     { connection =
         {| host = "localhost"
            port = 5432us
-           usr = "postgres"
-           pwd = "65a1561425f744e2b541303f628963f8"
-           using = "pilipala" |}
+           usr = cfg.db_user
+           pwd = cfg.pwd
+           using = cfg.db_name |}
       pooling = {| size = 32us; sync = 180us |}
       map =
         {| post = "schema.post"
